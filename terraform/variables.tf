1ec233d26d982881c6b9a91d3b4caed825d94917
Robert J. Calva <rcalvaga@redhat.com>
2021-10-04 14:54:15 -0500

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,15 +1,38 @@
-//variable "billing_account_id" {}
-
-variable "project_id" {
-  description = "The name of the new GCP project"
+variable "instance_name" {
+  type = "string"
 }
 
 variable "machine_type" {
-  description = "The machine type for compute instances use"
-  default     = "f1-micro"
+  description = "specified image id for instance"
+  default = "n1-standard-1"
 }
 
 variable "region" {
-  description = "GCP region"
-  default     = "us-east1"
+  description = "gcp region for specified instance"
+  default = "us-west1-b"
+}
+
+variable "project" {
+  description = "project name in which we will create all instance"
+  default = "cf-and-ansible-workshops"
+}
+
+variable "image_name" {
+  description = "image type for given instance"
+  default = "ubuntu-1604-xenial-v20190411"
+}
+
+variable "image_family" {
+  description = "image family for given instance"
+  default = "ubuntu-os-cloud"
+}
+
+variable "network" {
+  description = "network for given instance"
+  default = "default"
+}
+
+variable "tags" {
+  type = "list"
+  description = "tags is used for defining the rule of a instance"
 }
