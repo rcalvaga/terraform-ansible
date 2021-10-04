815c729dcb11625589afdfeb1393a3acc772863d
Robert J. Calva <rcalvaga@redhat.com>
2021-10-04 18:24:02 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,7 @@
 provider "google" {
   project     = var.project_id
   region      = var.region
+  credentials = file("/var/lib/awx/.config/gcloud/application_default_credentials.json")
 }
 
 locals {
